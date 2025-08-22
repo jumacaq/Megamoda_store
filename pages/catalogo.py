d65840c670789b20cc8886eb43c2ebcd35c0fe7d
Juan Campos Quintana <67085586+jumacaq@users.noreply.github.com>
2025-08-22 11:21:28 -0500

    Update catalogo.py

--- pages/catalogo.py
+++ pages/catalogo.py
@@ -340,8 +340,8 @@ def create_paypal_payment(items, user_id):
                 "payment_method": "paypal"
             },
             "redirect_urls": {
-                "return_url": "htpp://megamodastore.streamlit.app?payment=success",
-                "cancel_url": "http://megamodastore.streamlit.app?payment=cancelled"
+                "return_url": "htpps://megamodastore.streamlit.app?payment=success",
+                "cancel_url": "https://megamodastore.streamlit.app?payment=cancelled"
             },
             "transactions": [{
                 "item_list": {
